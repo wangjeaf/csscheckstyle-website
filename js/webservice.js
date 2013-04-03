4b3b0ef06ef9506fe0f6ac55a34eeb4535174d89
wangjeaf <wangjeaf@gmail.com>
2013-04-03 13:23:59 +0800

    aha~ english

--- js/webservice.js
+++ js/webservice.js
@@ -41,8 +41,8 @@
 	}
 	function buildContent(array) {
 		var result = ['<table width="100%" style="text-align:left;"><thead><tr><th width="40%">URL</th>\
-			<th>before</th><th>after</th><th>delta</th><th>% (delta/before)</th><th>Saved Per 10,000,000 PVs</th><th>compressed CSS</th><th>Replace/Recover CSS \
-				<a href="javascript:;" style="color:red;" title="Because compressed CSS is in CKstyle server, relative background image url will become INVALID.">(...)</a></th></tr></thead><tbody>'], current;
+			<th>before</th><th>after</th><th>delta</th><th>% (delta/before)</th><th>Saved Per 10,000,000 PVs</th><th>compressed CSS</th><th>Replace CSS \
+				<a href="javascript:;" style="color:red;" title="Because compressed CSS is placed in CKstyle server, relative background image url will become INVALID.">(!important)</a></th></tr></thead><tbody>'], current;
 		for (var i = 0, l = array.length; i < l; i ++) {
 			current = array[i];
 			current.delta = current.before - current.after;
