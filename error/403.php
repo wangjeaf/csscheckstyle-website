ea386a52591054fd17fcf035aa2b5b0f5f783bbb
wangjeaf <wangjeaf@gmail.com>
2013-04-12 18:44:16 +0800

    fix small bugs

--- error/403.php
+++ error/403.php
@@ -1,5 +1,5 @@
 <?php
-echo '<p>由于虚拟主机服务器资源有限，暂不开放实时CKstyle功能，敬请谅解 :-(</p>'.
+echo '<p>由于虚拟主机资源的严重不给力，暂未成功处理您的<strong>实时CKstyle</strong>请求，敬请谅解 :-(</p>'.
 	 '<p>强烈建议您先按照 <a href="javascript:;" data-dismiss="modal" onclick="$(\'a[href=#install]\').trigger(\'click\');">五步上手</a> 在本地试用一下，我相信您一定会爱上它的</p>'.
 	 '<p> O(∩_∩)O </p>';
 ?>
