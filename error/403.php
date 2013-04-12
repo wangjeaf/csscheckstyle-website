19f5eec3e02494c5f6b2c9fa1a77779c24acccb3
wangjeaf <wangjeaf@gmail.com>
2013-04-12 15:58:27 +0800

    ok

--- error/403.php
+++ error/403.php
@@ -1,5 +1,5 @@
 <?php
 echo '<p>由于虚拟主机服务器资源有限，暂不开放实时CKstyle功能，敬请谅解 :-(</p>'.
-	 '<p>强烈建议您先按照 <a href="javascript:;" data-dismiss="modal" onclick="$(\'a[href=#install]\').trigger(\'click\');">五步上手</a> 在本地先试用一下，我相信您一定会爱上它的</p>'.
+	 '<p>强烈建议您先按照 <a href="javascript:;" data-dismiss="modal" onclick="$(\'a[href=#install]\').trigger(\'click\');">五步上手</a> 在本地试用一下，我相信您一定会爱上它的</p>'.
 	 '<p> O(∩_∩)O </p>';
 ?>
