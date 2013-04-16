10c38fb97cd9dab9861d571787fca97a8dd8e1a0
wangjeaf <wangjeaf@gmail.com>
2013-04-16 16:02:32 +0800

    add normal intro

--- error/403.php
+++ error/403.php
@@ -1,5 +1,5 @@
 <?php
-echo '<p>由于虚拟主机资源的严重不给力，暂未成功处理您的<strong>实时CKstyle</strong>请求，敬请谅解 :-(</p>'.
+echo '<p>由于虚拟主机资源的严重不给力，暂不开放<strong>实时CKstyle</strong>功能，敬请期待 :-)</p>'.
 	 '<p>强烈建议您先按照 <a href="javascript:;" data-dismiss="modal" onclick="$(\'a[href=#install]\').trigger(\'click\');">五步上手</a> 在本地试用一下，我相信您一定会爱上它的</p>'.
 	 '<p>或者先看看CKstyle和各大CSS工具的 <a href="javascript:;" data-dismiss="modal" onclick="$(\'a[href=#pk]\').trigger(\'click\');">PK大战</a></p>'.
 	 '<p> O(∩_∩)O </p>';
